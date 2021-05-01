ec2dbfd8d94a0bdbb1efca1388e839e7b6e69f93
乐高易网络 <shengbo.gao@gmail.com>
2021-05-01 18:06:32 +0800

    Add files via upload

---
+++ elink-iov-exchange-client-processor/src/main/java/com/legaoyi/exchange/processor/handler/UrgentUpstreamMessageSendHandler.java
@@ -0,0 +1,30 @@
+package com.legaoyi.exchange.processor.handler;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.legaoyi.exchange.processor.util.ExchangeMessage;
+import com.legaoyi.mq.MQMessageProducer;
+/***
+ * 平台紧急下行消息，如链路控制消息
+ * @author 高胜波
+ *
+ */
+@Component("urgentUpstreamMessageSendHandler")
+public class UrgentUpstreamMessageSendHandler extends MessageHandler {
+
+    @Autowired
+    @Qualifier("upstreamMessageProducer")
+    private MQMessageProducer upstreamMessageProducer;
+
+    @Override
+    public void handle(ExchangeMessage message) throws Exception {
+        upstreamMessageProducer.send(message.getGatewayId().concat(".urgent"), message.toString());
+        if (this.getSuccessor() != null) {
+            this.getSuccessor().handle(message);
+        }
+    }
+
+}
