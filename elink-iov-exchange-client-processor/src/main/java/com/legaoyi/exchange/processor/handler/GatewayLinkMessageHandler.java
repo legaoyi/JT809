b029b927dea8105123ef17f50b9fe2799bb1be02
乐高易网络 <shengbo.gao@gmail.com>
2021-05-02 15:18:35 +0800

    Update GatewayLinkMessageHandler.java

--- elink-iov-exchange-client-processor/src/main/java/com/legaoyi/exchange/processor/handler/GatewayLinkMessageHandler.java
+++ elink-iov-exchange-client-processor/src/main/java/com/legaoyi/exchange/processor/handler/GatewayLinkMessageHandler.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 import com.legaoyi.exchange.processor.util.ExchangeMessage;
 /**
  * 网关中间件链路管理消息通知
- * @author 高胜波
+ * @author gaoshengbo
  *
  */
 @Component("gatewayLinkMessageHandler")
@@ -33,7 +33,10 @@ public class GatewayLinkMessageHandler extends MessageHandler {
         if (exchangeMessage.getMessageId().equals(ExchangeMessage.MESSAGEID_GATEWAY_LINK_STATUS_MESSAGE)) {
             Map<?, ?> data = (Map<?, ?>) exchangeMessage.getMessage();
             logger.info("******链路管理消息,link state message,message={}", data);
-            Long gnssCenterId = Long.parseLong(String.valueOf(data.get("gnssCenterId")));
+            Object accessCode = data.get("gnssCenterId");//2019版本
+            accessCode = (accessCode == null? data.get("accessCode"):accessCode);//2011版本
+            
+            Long gnssCenterId = Long.parseLong(String.valueOf(accessCode));
             Integer linkType = Integer.parseInt(String.valueOf(data.get("linkType")));
             Integer result = Integer.parseInt(String.valueOf(data.get("result")));
             if (linkType == 0) {
@@ -56,7 +59,8 @@ public class GatewayLinkMessageHandler extends MessageHandler {
             if (!isMainLinkActive && !isSubordinateLinkActive) {
                 // 链路断开之后，自动重连链路,开启主链路（同时会向上级平台发送主链路登录消息）,注意当上级平台禁止连接时会导致死循环，谨慎使用
                 Map<String, Object> map = new HashMap<String, Object>();
-                map.put("gnssCenterId", gnssCenterId);
+                map.put("gnssCenterId", gnssCenterId);//2019版本
+                map.put("accessCode", gnssCenterId);//2011版本
                 map.put("action", 3);
                 //urgentDownMessageSendHandler
                 //        .handle(new ExchangeMessage(ExchangeMessage.MESSAGEID_EXCHANGE_LINK_STATUS_MESSAGE, map, "", exchangeMessage.getGatewayId()));
